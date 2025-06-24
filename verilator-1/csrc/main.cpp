65b65de44df64bc3f6f6d9c518ba46c9f1a38601
徐海涛 <3544126384@qq.com>
2025-06-24 12:17:26 +0800

    二位四选一选择器

---
+++ verilator-1/csrc/main.cpp
@@ -0,0 +1,22 @@
+#include "Vmux41.h"
+#include "verilated.h"
+#include <nvboard.h>
+
+static TOP_NAME dut;
+
+void nvboard_bind_all_pins(Vmux41* top);
+
+static void single_cycle(){
+dut.eval();
+}
+
+int main(int argc,char** argv){
+nvboard_bind_all_pins(&dut);
+nvboard_init();
+
+while(1){
+nvboard_update();
+single_cycle();
+}
+return 0;
+}
